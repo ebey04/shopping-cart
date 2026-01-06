f5d53a8e80ed6deea728fe7c75d33b2add364de0
Elizabeth Bey <ebey04@gmail.com>
2026-01-06 16:15:39 -0500

    feat(main): create router for 3 pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router";
 import './index.css'
 import App from './App/App'
+import Cart-Page from './Cart Page'
+import Homepage from './Homepage'
+import Shop-Page from './Shop Page'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {index: true, element: <Homepage />},
+      {path: "shop", element: <Shop-Page />},
+      {path: "cart", element: <Cart-Page />,}
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </StrictMode>,
 )
