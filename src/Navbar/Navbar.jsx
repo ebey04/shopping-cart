f50cd72d20bc95dd94e365fd94f856f2c9cd21a4
Elizabeth Bey <ebey04@gmail.com>
2026-01-06 16:52:47 -0500

    feat(Navbar): add Grid and Flex to display

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -5,10 +5,13 @@ import cart from "../assets/cart.svg"
 export default function Navbar () {
 
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/shop">Shop Jewelry</Link>
-            <Link to="/cart"><img src={cart} alt="shopping cart icon" /></Link>
-        </nav>
+        <div className={styles.container}>
+            <h1>Mystic Jewels</h1>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/shop">Shop Jewelry</Link>
+                <Link to="/cart"><img src={cart} alt="shopping cart icon" /></Link>
+            </nav>
+        </div>
     )
 }
