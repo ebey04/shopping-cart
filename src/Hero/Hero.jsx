3c57abf6b64113354639d886b43b349b8c42d0ca
Elizabeth Bey <ebey04@gmail.com>
2026-01-07 13:39:11 -0500

    feat(hero): add header, style with blurr & gradient, use clamp() on font-size

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -1,1 +1,12 @@
 import styles from "./hero.module.css"
+import heroImg from "../assets/hero.segal.jpg"
+
+export default function Hero () {
+
+    return (
+        <div className= {styles.hero}> 
+            <h2>Ice Yourself</h2>
+            <img src= {heroImg} alt="Necklaces around a woman's neck" />
+        </div>
+    )
+}
--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -1,5 +1,12 @@
 import styles from "./homepage.module.css"
+import Navbar from "../Navbar/Navbar"
+import Hero from "../Hero/Hero"
 
 export default function Homepage () {
     
+    return (
+        <>
+            <Hero />
+        </>
+    )
 }
