002c4e71cf7771eacfc396c683565dbfa11cf26a
Elizabeth Bey <ebey04@gmail.com>
2026-01-07 15:28:41 -0500

    feat(footer): create and style simple footer with nav links

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import './App.css'
 import Navbar from '../Navbar/Navbar';
 import Button from '../Button/Button';
+import Footer from '../Footer/Footer';
 
 function App() {
 
@@ -27,7 +28,7 @@ function App() {
     <>
       <Navbar cartTotal={cartTotal} />
       <Outlet />
-      {/* Footer */}
+      <Footer />
     </>
   )
 }
--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,1 +1,16 @@
 import styles from "./footer.module.css"
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+
+    return(
+        <div className={styles.footer}>
+            <h4 className={styles.info}>Design by Elizabeth Bey 2026 ©</h4>
+            <div className= {styles.footLinks}>
+                <Link to="/">Home</Link>
+                <Link to="/shop">Shop</Link>
+                <Link to="/cart">Cart</Link>
+            </div>
+        </div>
+    )
+}
