69181e17824ee92d3e1e22fab2ed0d953e49ccf0
Elizabeth Bey <ebey04@gmail.com>
2026-01-09 11:45:14 -0500

    fix(card/product-grid): set aspect-ratio, fixed heights and centering for true retail look

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -6,11 +6,13 @@ export default function Card({ imgURL, titleEl, priceEl, descriptionEl }) {
     return (
         imgURL && ( 
         <div className={styles.card}>
-            <img src={imgURL} alt="jewelry pieces" />
+            <div className={styles.media}> 
+                <img src={imgURL} alt="jewelry pieces" />
+            </div>
             <div className={styles.contents}>
             <p data-testid="product-title" className={styles.title}>{titleEl}</p>
             <p data-testid="product-price" className={styles.price}>${priceEl}</p>
-            <p data-testid="product-description" className={styles.description}>{descriptionEl}</p>
+            {/* <p data-testid="product-description" className={styles.description}>{descriptionEl}</p> */}
             </div>
             <div className= {styles.btn}><Button>Add to Cart</Button> </div>
         </div>
