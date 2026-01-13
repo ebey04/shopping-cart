69d984afdf66047d4918910d83cecd8be4534d6c
Elizabeth Bey <ebey04@gmail.com>
2026-01-13 11:09:40 -0500

    feat(cartItem): add remove from cart button and style per Grid allowances

--- src/Cart Item/CartItem.jsx
+++ src/Cart Item/CartItem.jsx
@@ -1,26 +1,30 @@
-import styles from "./cart-item.module.css"
-import { useOutletContext } from 'react-router-dom';
-import Button from "../Button/Button"
+import styles from "./cart-item.module.css";
+import { useOutletContext } from "react-router-dom";
+import Button from "../Button/Button";
 
-export default function CartItem({item}) {
-    const { products, cartItems, increment, decrement } = useOutletContext();
+export default function CartItem({ item }) {
+    const { increment, decrement, removeFromCart } = useOutletContext();
+
+    const quantity = item.quantity;
 
     return (
-        <div className= {styles.prodRow}>
-            <div className={styles.media}> 
-                <img src={item.image} alt="jewelry pieces" />
-            </div>
-            <div className={styles.qty}>
-                <Button onClick={() => decrement(item.id)}>-</Button>
-                <span>{item.quantity}</span>
-                <Button onClick={() => increment(item.id)}>+</Button>
-            </div>
-
-            <div className= {styles.subtotal}>
-                <p>Subtotal: ${item.price * item.quantity}</p>
-            </div>
+        <div className={styles.prodRow}>
+        <div className={styles.media}>
+            <img src={item.image} alt="jewelry pieces" />
         </div>
 
-
-    )
+        {quantity > 0 ? (
+            <div className={styles.qty}>
+                <Button onClick={() => decrement(item.id)}>-</Button>
+                <span>{quantity}</span>
+                <Button onClick={() => increment(item.id)}>+</Button>
+            </div>
+        ) : (
+            <Button className={styles.delete} onClick={() => removeFromCart(item)}>Remove from Cart</Button>
+        )}
+        <div className={styles.subtotal}>
+            <p>Subtotal: ${item.price * item.quantity}</p>
+        </div>
+        </div>
+    );
 }
